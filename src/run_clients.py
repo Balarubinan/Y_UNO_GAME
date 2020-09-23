3956efbd9e62b47a78113dd610cdee0e4d109ed7
Bala <sudhagouthirubi@gmail.com>
2020-09-23 09:40:07 -1200

    Added files for UNO_Game
    Signed-off-by: Bala <sudhagouthirubi@gmail.com>

---
+++ src/run_clients.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import font
+from tkinter import Tk
+from PIL import Image
+from src.multi_client import CustomPlayer
+
+for y in range(2):
+    x=CustomPlayer('127.0.0.1',5000,f'player{y+1}')
+    x.connect_to_host()
+    x.start_recv()
